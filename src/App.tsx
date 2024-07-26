a74b51d71334d5fb9140e95cd699960d455a7376
JDiazc0 <diaz.jhoan@correounivalle.edu.co>
2024-07-26 15:22:47 -0500

    feat (home): Add home in file "App.tsx"

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
+import Home from "./components/Home";
 import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <>
       <Navbar />
-      <h1 className="font-Signatra text-8xl  text-primary-500">
-        Bienvenidos al himalaya
-      </h1>
+      <Home />
     </>
   );
 }
