8ba51ff400b27e92929fdb4e86378d351f3d309d
JDiazc0 <diaz.jhoan@correounivalle.edu.co>
2024-07-28 15:59:30 -0500

    style: Add background

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import AboutUs from "./components/AboutUs";
 import Benefits from "./components/Benefits";
+import Footer from "./components/Footer";
 import Home from "./components/Home";
 import Navbar from "./components/Navbar";
 import Products from "./components/Products";
@@ -8,11 +9,12 @@ function App() {
   return (
     <>
       <Navbar />
-      <div className="pt-20">
+      <div className="pt-20 bg-gradient-to-b from-white from-80% to-primary-50">
         <Home />
         <AboutUs />
         <Products />
         <Benefits />
+        <Footer />
       </div>
     </>
   );
